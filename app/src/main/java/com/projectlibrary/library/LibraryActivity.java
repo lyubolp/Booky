e9e2f67297b2c05baedfa162880b0fdbf25cc83e
lyubolp <lyubo_karev@abv.bg>
2018-05-07 23:56:09 +0300

    Added library activity

---
+++ app/src/main/java/com/projectlibrary/library/LibraryActivity.java
@@ -0,0 +1,34 @@
+package com.projectlibrary.library;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class LibraryActivity extends AppCompatActivity {
+
+    //Function that loads an image based on a URL (may be usefull in the future)
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+
+    //Events for images clicked
+
+    
+}
